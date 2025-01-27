1c4a1018f2c20e41aa43ec83db6ca3d3beff1ca1
Preston Connors <prestonconnors@gmail.com>
2025-01-26 20:16:30 -0500

    updated port number

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 # Gunicorn configuration file: gunicorn_config.py
-bind = "127.0.0.1:8000"  # Bind to localhost on port 8000
+bind = "127.0.0.1:10929"  # Bind to localhost on port 10929
 workers = 3  # Number of worker processes to handle requests
 threads = 2  # Number of threads per worker
 worker_class = "gthread"  # Use the gthread worker for thread-based concurrency
